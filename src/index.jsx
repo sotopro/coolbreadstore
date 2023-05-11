24146be115ebee7f15a972bdd6221b60f4802ad9
Daniel Soto <dasg.1993@gmail.com>
2023-05-10 20:02:36 -0500

    [feature] - add react navigation deps and implement app navigator

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { useFonts } from "expo-font";
 import { ActivityIndicator, View } from "react-native";
 
-import { Categories } from "./screens";
+import AppNavigator from "./navigation";
 import { styles } from "./styles";
 
 export default function App() {
@@ -24,5 +24,5 @@ export default function App() {
       </View>
     );
   }
-  return <Categories />;
+  return <AppNavigator />;
 }
---
+++ src/navigation/main/index.jsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Categories, Product, Products } from "../../screens";
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Categories">
+      <Stack.Screen name="Categories" component={Categories} />
+      <Stack.Screen name="Products" component={Products} />
+      <Stack.Screen name="Product" component={Product} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigator;
