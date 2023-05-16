dabbab1ec89e849ce5b36b4bc7b57aec87ba8af6
Daniel Soto <dasg.1993@gmail.com>
2023-05-15 19:10:39 -0500

    [feature] - fix padding top for category screen in android

--- src/screens/categories/styles.js
+++ src/screens/categories/styles.js
@@ -1,9 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
   },
   title: {
     fontSize: 20,
