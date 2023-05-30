5950efca3a1ee2d8d8592a77cd3b82f9f7d4431d
Daniel Soto <dasg.1993@gmail.com>
2023-05-29 20:02:32 -0500

    [feature] - create layout for auth login/register flow and styles

--- src/screens/auth/index.jsx
+++ src/screens/auth/index.jsx
@@ -1,11 +1,50 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
+import { COLORS } from "../../constants";
 
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const title = isLogin ? "Login" : "Register";
+  const buttonTitle = isLogin ? "Login" : "Register";
+  const messageText = isLogin ? "Don't have an account?" : "Already have an account?";
+
+  const onHandleChangeAuth = () => {
+    setIsLogin(!isLogin);
+  };
   return (
     <View style={styles.container}>
-      <Text>Auth</Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="email@gmail.com"
+          placeholderTextColor={COLORS.darkGray}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={() => null}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="********"
+          placeholderTextColor={COLORS.darkGray}
+          secureTextEntry
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={() => null}
+        />
+        <View style={styles.linkContainer}>
+          <TouchableOpacity style={styles.link} onPress={onHandleChangeAuth}>
+            <Text style={styles.linkText}>{messageText}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.submitContainer}>
+          <Button title={buttonTitle} color={COLORS.text} onPress={() => null} />
+        </View>
+      </View>
     </View>
   );
 };
